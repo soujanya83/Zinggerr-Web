88d87f80bb72c47b21c8bf2563e31f556a44c16d
Sagar <sagar@qubetadigital.com.au>
2024-12-19 08:53:15 +0530

    role to type

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->string('password');
             $table->text('profile_picture')->nullable();
             $table->enum('gender', ['Male', 'Female', 'Other'])->nullable();
-            $table->enum('role', ['Superadmin', 'Admin', 'Teacher', 'Student', 'Staff'])->default('Superadmin');
+            $table->enum('type', ['Superadmin', 'Admin', 'Teacher', 'Student', 'Staff'])->default('Superadmin');
             $table->rememberToken();
             $table->timestamps();
         });
