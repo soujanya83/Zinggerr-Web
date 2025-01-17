68266de158c9341e6ac9e27c95357a0e6af68344
chandankumarsingh1176 <chandantafi1996@gmail.com>
2025-01-17 17:29:28 +0530

    courses assign and permissions

--- resources/views/courses/add.blade.php
+++ resources/views/courses/add.blade.php
@@ -449,7 +449,7 @@
 
                                             <div class="form-check form-check-inline">
                                                 <input class="form-check-input" type="checkbox" value="Basic" id="basic"
-                                                    name="tags[]" required>
+                                                    name="tags[]">
                                                 <label class="form-check-label" for="basic"
                                                     style="color: black">Basic</label>
                                             </div>
