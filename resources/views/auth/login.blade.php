e8c1a9c361b13111ca393028473985fe640524a5
chandankumarsingh1176 <chandantafi1996@gmail.com>
2025-03-04 10:23:45 +0530

    dashboard design correction

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,7 +23,7 @@
 
 @foreach ($errors->all() as $error)
 {{-- <div class="alert alert-danger">{{ $error }}</div> --}}
-<div class="alert alert-danger alert-dismissible fade" role="alert">
+<div class="alert alert-danger alert-dismissible  fade show" role="alert">
     {{ $error }}
     <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
 </div>
