3651290beb1eaa67a2b70cba1f1bc76aa6b54537
chandankumarsingh1176 <chandantafi1996@gmail.com>
2025-03-18 11:37:22 +0530

    courses views bugs fixed

--- routes/web.php
+++ routes/web.php
@@ -281,7 +281,7 @@ Route::middleware(['web', 'auth', ClearCacheAfterLogout::class])->group(function
     Route::get('students', [StudentController::class, 'studentlist'])->name('studentlist');
 
     // Route::put('/teacher/{id}/change-status', [TeacherController::class, 'changeStatus'])->name('change_teacher_status');
-    Route::get('courses/{slug}/view', [StudentController::class, 'courses_views'])->name('courses.viwes');
+    Route::get('courses/{slug}', [StudentController::class, 'courses_views'])->name('courses.viwes');
 
 
     // Route::delete('/teacher-delete/{id}', [TeacherController::class, 'teacher_delete'])->name('teacher_delete')->middleware('can:role');
