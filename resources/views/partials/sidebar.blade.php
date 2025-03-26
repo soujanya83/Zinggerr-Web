5687a13102d3d186bc6473a9eef25dda54f22dc5
chandankumarsingh1176 <chandantafi1996@gmail.com>
2025-03-26 10:43:45 +0530

    in sidebar changes

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -96,7 +96,7 @@
                         @endphp
                        <li class="pc-item pc-hasmenu">
                         <a href="#" class="pc-link">
-                            <span class="pc-mtext" data-i18n="Montessori">Casa Dei Bambini (3-6 years)</span>
+                            <span class="pc-mtext" data-i18n="Montessori">Casa dei Bambini (3-6 years)</span>
                             <span class="pc-arrow"><i data-feather="chevron-right"></i></span>
                         </a>
                         <ul class="pc-submenu">
